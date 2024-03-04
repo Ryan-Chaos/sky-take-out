251c731c09e8b38a0d2840672a94c4c0a30070c4
Ryan-Chaos <1527124085@qq.com>
2024-03-04 17:03:01 +0800

    补充-订单支付功能

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -22,4 +22,13 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+
+    /**
+     * 根据id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long userId);
 }
